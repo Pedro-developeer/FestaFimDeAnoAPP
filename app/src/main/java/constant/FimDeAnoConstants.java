38a8bfa7e31262eccc848ffb0ccf5d2ccae2a8f4
Pedrto henrique <pedromonoryze@gmail.com>
2021-07-07 20:02:26 -0300

    Initial commit

---
+++ app/src/main/java/constant/FimDeAnoConstants.java
@@ -0,0 +1,1 @@
+package constant;public class FimDeAnoConstants {    public static String PRESENCE_KEY = "presencekey";    public static String CONFIRMATION_YES = "CONFIRMATION_YES";    public static String CONFIRMATION_NO = "CONFIRMATION_NO";}
